de8d41840c400a87ebb3b537cc6fedeac61e1343
Satya2444 <70252876+Satya2444@users.noreply.github.com>
2020-08-26 16:38:30 +0530

    Create ObjectConst.js

---
+++ ObjectConst.js
@@ -0,0 +1,10 @@
+function createuser(name,occ){
+  this.name=name;
+  this.occupation=occupation;
+}
+
+let user1 = new createuser('Satya','SE');
+let user2 = new createuser('satya1','PL');
+
+console.log(user1);
+console.log(user2);
