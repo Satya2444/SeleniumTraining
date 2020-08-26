640764c8a111be30b58039636f25db9be3da99bd
Satya2444 <70252876+Satya2444@users.noreply.github.com>
2020-08-26 11:57:05 +0530

    Create Arrays.js

---
+++ Arrays.js
@@ -0,0 +1,7 @@
+var usrdetails = [3,300,"satya"];
+console.log(usrdetails[0])
+console.log(usrdetails[1])
+console.log(usrdetails[2])
+console.log(usrdetails.length);
+usrdetails.push("some")
+console.log(usrdetails)
